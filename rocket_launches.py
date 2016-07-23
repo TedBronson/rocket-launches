26dc58f8eed66dc8ae6eef7ed543b182498661e0
TedBronson <kabashnyiyaroslav@gmail.com>
2016-07-23 15:08:46 +0300

    Got launches in next x days with description

--- rocket_launches.py
+++ rocket_launches.py
@@ -53,18 +53,23 @@ def number_of_launches_per_family(rocket_families_list):
         print(rocket_family + ' was launched: ' + str(number_of_launches) + ' times')
 
 
-def launches_in_period(today_date='2016-07-22', future_date='2016-07-30'):  # Add actual calculated dates here
+def launches_in_period(today_date='2016-07-22', future_date='2016-08-30'):  # Add actual calculated dates here
     upcoming_launches = r.get('https://launchlibrary.net/1.2/launch/{0}/{1}'.format(today_date, future_date))
     upcoming_launches_dict = upcoming_launches.json()
     if upcoming_launches_dict['count'] > 0:
         upcoming_launches_number = upcoming_launches_dict['count']
-        launch_dict = {}
-        launch_dict['launch_name'] = upcoming_launches_dict['launches'][0]['name']
-        launch_dict['net'] = upcoming_launches_dict['launches'][0]['net']
-        launch_dict['vidURLs'] = upcoming_launches_dict['launches'][0]['vidURLs']
-        launch_dict['location_name'] = upcoming_launches_dict['launches'][0]['location']['pads'][0]['name']
-        launch_dict['rocket_name'] = upcoming_launches_dict['launches'][0]['rocket']['name']
-        launch_dict['mission_description'] = upcoming_launches_dict['launches'][0]['missions'][0]['description']
+        launch_list = []
+        for i in range(0, upcoming_launches_number):
+            launch_dict = {}
+            launch_dict['launch_name'] = upcoming_launches_dict['launches'][i]['name']
+            launch_dict['net'] = upcoming_launches_dict['launches'][i]['net']
+            launch_dict['vidURLs'] = upcoming_launches_dict['launches'][i]['vidURLs']
+            launch_dict['location_name'] = upcoming_launches_dict['launches'][i]['location']['pads'][0]['name']
+            launch_dict['rocket_name'] = upcoming_launches_dict['launches'][i]['rocket']['name']
+            if ['missions'][0] in upcoming_launches_dict.keys():
+                launch_dict['mission_description'] = upcoming_launches_dict['launches'][i]['missions'][0]['description']
+            launch_list.append(launch_dict)
+        list_of_upcoming_launches = launch_list
     else:
         # Message for no launches
         list_of_upcoming_launches = ['No upcoming launches']
@@ -78,9 +83,11 @@ def main():
     # number_of_pages = get_number_of_pages(base_url, rocket_family)
     # get_list_of_launches(base_url, rocket_family, number_of_pages)
 
-    rocket_families_list = get_list_of_rocket_families()
+    print (launches_in_period())
 
-    number_of_launches_per_family(rocket_families_list)
+    # rocket_families_list = get_list_of_rocket_families()
+    #
+    # number_of_launches_per_family(rocket_families_list)
 
 
 main()
