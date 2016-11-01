007fae2ae47cf2d653368591e0022d80a87aa234
Yaroslav Kabashnyi <kabashnyiyaroslav@gmail.com>
2016-11-01 13:24:39 +0200

    Updated email with MIME

--- launch_library.py
+++ launch_library.py
@@ -1,12 +1,8 @@
-import requests as r
-import math
-import encodings
-
 from dask.compatibility import FileNotFoundError
 
-import rocket_launches
 import smtplib
 import rocket_launches_in_period
+from email.mime.text import MIMEText
 
 
 def send_email(message):
@@ -28,14 +24,17 @@ def send_email(message):
     server.login(from_email, password)
 
     # Compose a message
-    message_constructed = 'Subject: Rocket launches for today \n'
-    message_constructed += 'In next 3 days we are expecting following launches: \n \n'
+    message_constructed = 'In next 3 days we are expecting following launches: \n \n'
     message_constructed += message
+
+    msg = MIMEText(message_constructed)
+    msg['Subject'] = 'Rocket launches for today'
+    msg['From'] = from_email
 
     # Send a message
     to_email = content[2]
-    server.sendmail(from_email, to_email, message_constructed)
-    print(message_constructed)
+    server.sendmail(from_email, to_email, msg.as_string())
+    print(msg.as_string())
 
     server.quit()
 
